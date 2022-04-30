9778e6aaa1456e1e250c493d0ad76b6f41aa2886
Thushalya <weeeratharava@gmail.com>
2022-04-30 09:57:04 +0530

    first commit

---
+++ BACKEND/server.js
@@ -0,0 +1,6 @@
+const express =require("express");
+const mongoose =require ("mongoose");
+const bodyParser =require ("body-parser");
+const cors =require ("cors");
+const dotenv =require("dotenv");
+const app =express();
